6fcc2f853733ad364a1f6cdf633cd274784222c0
Tad seldovia <tadeze@live.com>
2018-07-24 14:39:39 -0700

    mixlinear model fixed.

--- model/hurdle_regression.py
+++ model/hurdle_regression.py
@@ -1,23 +1,7 @@
 import numpy as np
-import statsmodels.api as st
-import statsmodels.formula.api as sfa
 from sklearn.neighbors import KernelDensity
 from sklearn.linear_model import LinearRegression, HuberRegressor
-from sklearn.base import BaseEstimator
 from sklearn.linear_model.logistic import LogisticRegression
-
-class Regressor(BaseEstimator):
-    def __init__(self):
-        self.clf = LinearRegression()
-    def fit(self, X,y):
-        self.clf.fit(X,y)
-    def predict(self, X):
-        return self.clf.predict(X)
-
-
-
-
-kde = KernelDensity()
 
 class MixLinearModel(object):
     """
@@ -27,8 +11,10 @@ class MixLinearModel(object):
 
     def __init__(self):
         self.reg_model = LinearRegression()
-        self.hurdle_model = sfa.GLM()
+
+        self.eps = 0.001
         self.log_reg = LogisticRegression()
+        self.kde = KernelDensity(kernel="gaussian")
     def train(self, y, x):
         """
 
@@ -39,9 +25,14 @@ class MixLinearModel(object):
         Returns:
 
         """
-        zero_one = np.int(y>0)
-        self.log_reg.fit(x,zero_one)
-        self.reg_model.fit(X=x,y=zero_one, sample_weight=self.log_reg.fit_intercept)
+        ## fit regression on log scale.
+        y = y.reshape(-1,1)
+
+        zero_one = (y>0.5).astype(int)
+        self.log_reg.fit(x, zero_one)
+        self.reg_model.fit(X=np.log(x+self.eps),y=np.log(y+self.eps))
+        res = (y - self.reg_model.predict(np.log(x+self.eps)))
+        self.kde.fit(res)
 
         ## Train the hurdle model and use the fitted value to train the regression model.
         # - train reg_model
@@ -51,13 +42,15 @@ class MixLinearModel(object):
         Returns:
 
         """
+        pass
 
-    def predict(self, x):
-        p = self.hurdle_model.fit(x)
-        linear_pred = self.reg_model.predict(x)
-        return self.__mixl(x, p, linear_pred)
+    def predict(self, y, x):
+        p = self.log_reg.predict(x)
+        linear_pred = self.reg_model.predict(np.log(x+self.eps))
+        return self.__mixl(y, p, linear_pred)
 
-    def __mixl(self, x, p, linear_predictions):
+
+    def __mixl(self, y, p, linear_predictions):
         """
          - if RAIN = 0, $ -log (1-p_1)$
          - if RAIN > 0, $ -log [p_1 \frac{P(log(RAIN + \epsilon)}{(RAIN + \epsilon)}]$
@@ -71,16 +64,34 @@ class MixLinearModel(object):
 
         """
         # Reshape for 1-D format.
-        eps = 0.001
+        #eps = 0.001
         p = p.reshape([-1, 1])
-        observations = x.reshape([-1, 1])
+        observations = y.reshape([-1, 1])
         predictions = linear_predictions.reshape([-1, 1])
 
         zero_rain = np.multiply((1 - p), (observations == 0))
         non_zero = np.divide(np.multiply(p,
-                                         np.exp(kde.score_samples(predictions - np.log(observations + eps))).reshape(
+                                         np.exp(self.kde.score_samples(predictions - np.log(observations + self.eps))).reshape(
                                              [-1, 1])),
-                             abs(observations + eps))
+                             abs(observations + self.eps))
 
         result = zero_rain + non_zero
         return result
+
+if __name__ == '__main__':
+    #import pandas as pd
+    #df = pd.read_csv('../gpm-tahmo-rain.csv')
+    w = np.random.rand(100,6)
+
+    y ,x = w[:,0], w[:,1:]
+    #print y
+    mixl = MixLinearModel()
+    mixl.reg_model.fit(x,y)
+    yz = (y>0.5).astype(int)
+    mixl.log_reg.fit(x, yz)
+    res = y.reshape(-1,1) - mixl.reg_model.predict(x).reshape(-1,1)
+    #print res, res.shape
+    #mixl.kde.fit(res)
+    #print mixl.kde.score_samples(y.reshape(-1,1))
+    mixl.train(y, x)
+    print mixl.predict(y, x)
